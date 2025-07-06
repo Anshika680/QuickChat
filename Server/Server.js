5725d337386d70d0c9800002e6a1fed8145931e1
Anshika680 <anshikagupta23106@gmail.com>
2025-07-06 18:11:31 +0530

    updated server

--- Server/Server.js
+++ Server/Server.js
@@ -46,7 +46,13 @@ app.use('/api/status', (req, res) => {
 app.use('/api/auth', userRouter);
 app.use('/api/messages', messageRouter);
 
+
+if (process.env.NODE_ENV !== 'production') {
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () =>
     console.log(`Server is listening on ${PORT}`)
 );
+}
+
+export default server;
